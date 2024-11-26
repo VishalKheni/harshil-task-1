82f2733f7e0333bfd2bae5536e8f5e50500ef64f
harshilcubesdev <harshil.cubesdev@gmail.com>
2024-11-26 14:51:48 +0530

    Initial commit

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,18 +7,17 @@ const verifyToken = async (req, res, next) => {
         const token = req.header("authorization")?.replace("Bearer ", "")
 
         if (!token) {
-            return res.status(400).send({ error: true, message: "Invalid use of bearer auth token" });
+            return res.status(400).send({ message: "Invalid use of bearer auth token" });
         }
     
         const user = jwt.verify(token, accessskey);
         if (!user) {
-            return res.status(401).send({ error: true, message: "Unauthorized" });
+            return res.status(401).send({  message: "Unauthorized" });
         }
         req.user = user;
         next();
     } catch (err) {
         return res.status(500).json({
-            error: true,
             message: 'Internal Server Error',
         });
 }
