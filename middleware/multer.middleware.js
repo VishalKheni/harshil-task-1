91d4073152604412ec525f1840a40656f6c434c2
harshilcubesdev <harshil.cubesdev@gmail.com>
2024-12-03 18:31:51 +0530

    change 3-12, time-6:31

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,30 +1,31 @@
-const multer  = require('multer');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-
+const multer = require("multer");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public'); 
+    cb(null, "public");
   },
   filename: (req, file, cb) => {
     const extname = path.extname(file.originalname).toLowerCase();
-    const imageId = uuidv4();;
-    cb(null, imageId + extname); 
+    const imageId = uuidv4();
+    cb(null, imageId + extname);
   },
 });
 
 const upload = multer({
   storage: storage,
+  limits: { fileSize: 1024 * 1024 * 2 },
   fileFilter: (req, file, cb) => {
     const extname = path.extname(file.originalname).toLowerCase();
-    if (['.jpg', '.jpeg', '.png'].includes(extname)) {
+    if ([".jpg", ".jpeg", ".png"].includes(extname)) {
       cb(null, true);
     } else {
-      cb(new Error('Invalid file type. Only JPG, JPEG, PNG images are allowed.'));
+      cb(
+        new Error("Invalid file type. Only JPG, JPEG, PNG images are allowed.")
+      );
     }
   },
 });
-  
-  module.exports = upload;
-  
+
+module.exports = upload;
